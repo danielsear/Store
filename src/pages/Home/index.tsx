c6044be069c68ed46b99ced8a4f2a88e8275db56
Danielsear <danielandradeseara@hotmail.com>
2022-05-14 12:39:49 -0300

    created footer component and its styling

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import Footer from '../../components/Footer'
 import Header from '../../components/Header'
 import './styles.css'
 
@@ -6,6 +7,7 @@ function Home(){
   return (
     <>
       <Header/>
+      <Footer/>
     </>
   )
 }
