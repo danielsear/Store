b431d4fc1fb644c75f5c38ea6880faa604cda2ba
Danielsear <danielandradeseara@hotmail.com>
2022-05-14 08:15:45 -0300

    init

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import './styles.css'
+
+
+function Header(){
+  return (
+    <>
+      <div>
+      <section>Kassinha Variedades</section>
+      <section>Pesquisa</section>
+      <section></section>
+      </div>
+    </>
+  )
+}
+
+export default Header
+
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import Header from '../../components/Header'
+import './styles.css'
+
+
+function Home(){
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+
+export default Home
