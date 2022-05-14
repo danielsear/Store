2371df515d77d4cf54095729a062cbde5d48630f
Danielsear <danielandradeseara@hotmail.com>
2022-05-14 09:22:17 -0300

    initializing the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,23 @@
 import './styles.css'
 
+import userMenu from '../../assets/images/user-enter1.svg'
 
 function Header(){
   return (
-    <>
-      <div>
-      <section>Kassinha Variedades</section>
-      <section>Pesquisa</section>
-      <section></section>
+    
+      <div id='header_container'>
+        <section className='logo'>Kassinha Variedades</section>
+        <section className='pesquisa'>Pesquisa</section>
+        <section className='user_menu'>
+          <div >
+            <a href="#userMenu" >
+              <i><img src={userMenu} alt="Menu do usuário" /></i>
+              Entrar
+            </a>
+          </div>
+        </section>
       </div>
-    </>
+    
   )
 }
 
