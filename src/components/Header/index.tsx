3ac46bf5366043ce71dc973f238b2c085fae6a32
Danielsear <danielandradeseara@hotmail.com>
2022-05-15 15:47:26 -0300

    creating menu to login user and finished authentication by google account

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,19 +7,46 @@ import {FormEvent} from 'react'
 
 import { useNavigate } from 'react-router-dom'
 
+import useAuth from '../../hooks/useAuth'
+import { database } from '../../services/firebase'
+
 
 
 
 function Header(){
   const navigate = useNavigate()
+  const {user,signInWithGoogle} = useAuth()
+
+  
 
   function handleLogin(e: FormEvent) {
     e.preventDefault()
 
-    navigate('/form')
+    navigate('/form/login')
+  }
+  async function handleLoginGoogle(e: FormEvent) {
+    e.preventDefault()
+
+    if(!user){
+      await signInWithGoogle()
+    }
+  }
+ 
+  function handleLogoff(){
+  }
+  
+
+  function handleShowMenuOver(){  
+    const fild = document.querySelector('.show_hidden')
+      fild?.classList.add('show_menu')
+  }
+  function handleShowMenuOut(){  
+    setTimeout(() => {
+      const fild = document.querySelector('.show_hidden')
+      fild?.classList.remove('show_menu')
+    }, 3000);
   }
 
-  
   return (
       <div id='header_container'>
         <section className='logo'>Kassinha Variedades</section>
@@ -30,12 +57,28 @@ function Header(){
            </div>
           </section>
         <section className='user_menu'>
-          <div onClick={handleLogin}>
+          {!user ? (
+            <>
+            <div  onMouseOver={handleShowMenuOver} onMouseOut={handleShowMenuOut}>
             <a href="#userMenu" >
               <i><img src={userMenu} alt="Menu do usuário" /></i>
               Entrar
             </a>
           </div>
+          <div className='show_hidden'>
+                  <p onClick={handleLoginGoogle}>Google</p>
+                  <p onClick={handleLogin}>User</p>
+            </div>
+            </>
+          ): (
+            <div >
+              <img src={user.avatar} alt={user.name} />
+              Olá, seja bem vindo! 
+            <a href="#userMenu" className='sair' onClick={handleLogoff}>
+              Sair
+            </a>
+          </div>
+          )}
         </section>
       </div>
     
